5144e6a50905cbb900fd4225bb6a13375666deef
Curso de JS <matheuschiimenndes@gmail.com>
2024-08-04 20:00:37 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+
+import { Button } from "./_components/ui/button";
 
 export default function Home() {
-  return <h1 className=" mb-5 w-10  text-red-100 bg-red-500">Ola</h1>;
+  return (
+    <Button>Teste</Button>
+  )
+
 }
