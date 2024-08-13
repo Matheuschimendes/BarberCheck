128cac788983a57591dd61293552f6ce2819c285
Curso de JS <matheuschiimenndes@gmail.com>
2024-08-12 22:31:46 -0300

    chore: add next auth setup

--- src/app/_components/sidebar-sheet.tsx
+++ src/app/_components/sidebar-sheet.tsx
@@ -1,26 +1,60 @@
 import { Button } from "./ui/button"
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import {
+  CalendarIcon,
+  HomeIcon,
+  LogIn,
+  LogInIcon,
+  LogOutIcon,
+} from "lucide-react"
 import { SheetClose, SheetContent, SheetHeader } from "./ui/sheet"
 import { quickSearchOption } from "../_constante/search"
 import { Avatar, AvatarImage } from "./ui/avatar"
 import Link from "next/link"
 import Image from "next/image"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 
 const SidebarSheet = () => {
   return (
     <SheetContent className="overflow-y-auto">
       <SheetHeader className="text-left">Menu</SheetHeader>
 
-      <div className="flex items-center gap-3 border-b border-solid py-5">
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        <h2 className="font-bold">Olá, faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça seu login na plataforma.</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do google.
+              </DialogDescription>
+            </DialogHeader>
+            <Button variant="outline" className="gap-1 font-bold">
+              <Image src="/google.svg" alt="" width={18} height={18} />
+              google
+            </Button>
+          </DialogContent>
+        </Dialog>
         {/* Direita */}
-        <Avatar>
+        {/* <Avatar>
           <AvatarImage src="https://images.unsplash.com/photo-1543610892-0b1f7e6d8ac1?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8YXZhdGFyfGVufDB8fDB8fHww" />
         </Avatar>
-        {/* Esquerda */}
+        
         <div className="">
           <p className="font-bold">Matheus Chimendes</p>
           <p className="text-xs">chimendes@gmail.com</p>
-        </div>
+        </div> */}
       </div>
 
       <div className="flex flex-col gap-2 border-b border-solid py-5">
